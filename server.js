d0e42bf7fbf7098b43acb37b32bcdc50394ab75f
Robert Dunn <rcdunnii@gmail.com>
2014-12-02 14:08:40 -0600

    Setup handlebars example

--- server.js
+++ server.js
@@ -1,22 +1,85 @@
-var appRoot = require('app-root-path');
-var myModule = require(appRoot + '/server.js');
-var myModulePath = require('app-root-path').resolve('/public/test.html');
-// server.js (Express 4.0)
-var express        = require('express');
+/*var express = require('express');
+var app = express();
+var expressHbs = require('express3-handlebars');
+
+app.engine('hbs', expressHbs({extname:'hbs', defaultLayout:'main'}));
+app.set('view engine', 'hbs');
+
 var morgan         = require('morgan');
 var bodyParser     = require('body-parser');
 var methodOverride = require('method-override');
-var app            = express();
 
 app.use(express.static(__dirname + '/public')); 	// set the static files location /public/img will be /img for users
 app.use(morgan('dev')); 					// log every request to the console
-app.use(bodyParser.urlencoded({ extended: false }))    // parse application/x-www-form-urlencoded
-app.use(bodyParser.json())    // parse application/json
+app.use(bodyParser.urlencoded({ extended: false }));   // parse application/x-www-form-urlencoded
+app.use(bodyParser.json());    // parse application/json
 app.use(methodOverride()); 					// simulate DELETE and PUT
 
 app.listen(8080);	
 console.log("Magic happens on port 8080 with __dirname of %s", __dirname); 			// shoutout to the user
 
 app.get('/', function( req, res) {
-    res.send("Hello World");
+   res.render("home");
 });
+*/
+var express = require('express');
+var app = express();
+var expressHbs = require('express3-handlebars');
+
+app.engine('hbs', expressHbs({extname:'hbs', defaultLayout:'main.hbs'}));
+app.set('view engine', 'hbs');
+
+app.get('/', function(req, res){
+  res.render('index');
+});
+
+app.get('/simple', function(req, res){
+  var data = {name: 'Gorilla'};
+  res.render('simple', data);
+});
+
+app.get('/complex', function(req, res){
+  var data = {
+    name: 'Gorilla',
+    address: {
+      streetName: 'Broadway',
+      streetNumber: '721',
+      floor: 4,
+      addressType: {
+        typeName: 'residential'
+      }
+    }
+  };
+  res.render('complex', data);
+});
+
+app.get('/loop', function(req, res){
+  var basketballPlayers = [
+    {name: 'Lebron James', team: 'the Heat'},
+    {name: 'Kevin Durant', team: 'the Thunder'},
+    {name: 'Kobe Jordan',  team: 'the Lakers'}
+  ];
+  
+  var days = [
+    'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'
+  ];
+  
+  var data = {
+    basketballPlayers: basketballPlayers,
+    days: days
+  };
+  
+  res.render('loop', data);
+});
+
+app.get('/logic', function(req, res){
+  var data = {
+    upIsUp: true,
+    downIsUp: false,
+    skyIsBlue: "yes"
+  };
+  
+  res.render('logic', data);
+});
+
+app.listen(8080);
